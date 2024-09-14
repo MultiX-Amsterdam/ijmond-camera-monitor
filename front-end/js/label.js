dc5045e13fc5ed8a3d1d1d0291d5c90dfecd85bd
Yen-Chia Hsu <hsu.yenchia@gmail.com>
2024-09-14 12:12:24 +0200

    Remove the personal contribution button on the labeling page.

--- front-end/js/label.js
+++ front-end/js/label.js
@@ -74,7 +74,6 @@
   }
 
   function onUserIdChangeSuccess(new_user_id) {
-    $("#review").prop("href", "label-gallery.html" + "?user_id=" + new_user_id);
     if (is_first_time) {
       is_first_time = false;
       $next = $("#next");
